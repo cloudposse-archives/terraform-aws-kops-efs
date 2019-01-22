d550be554bc86451b284a0ef1498ef2c4b381886
Josh Myers <joshuajmyers@gmail.com>
2019-01-22 16:31:28 +0000

    Init kops-efs module for `efs-provider` IAM role
    This was largely taken from `terraform-aws-kops-external-dns` which does
    similar. Note that the change here is that we fall back onto the default
    `AmazonElasticFileSystemFullAccess` role rather than creating our own.

---
+++ main.tf
@@ -0,0 +1,60 @@
+module "label" {
+  source     = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.3.3"
+  namespace  = "${var.namespace}"
+  stage      = "${var.stage}"
+  name       = "${var.name}"
+  delimiter  = "${var.delimiter}"
+  attributes = "${var.attributes}"
+  tags       = "${var.tags}"
+}
+
+module "kops_metadata" {
+  source       = "git::https://github.com/cloudposse/terraform-aws-kops-metadata.git?ref=tags/0.1.1"
+  dns_zone     = "${var.cluster_name}"
+  masters_name = "${var.masters_name}"
+  nodes_name   = "${var.nodes_name}"
+}
+
+resource "aws_iam_role" "default" {
+  name        = "${module.label.id}"
+  description = "Role that can be assumed by efs-provider"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+
+  assume_role_policy = "${data.aws_iam_policy_document.assume_role.json}"
+}
+
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    actions = [
+      "sts:AssumeRole",
+    ]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+
+    principals {
+      type = "AWS"
+
+      identifiers = [
+        "${module.kops_metadata.masters_role_arn}",
+        "${module.kops_metadata.nodes_role_arn}",
+      ]
+    }
+
+    effect = "Allow"
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "default" {
+  role       = "${aws_iam_role.default.name}"
+  policy_arn = "${var.policy_arn}"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
